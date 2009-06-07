7c0b3f909401058d123d10ebd80e84c991c4b256
Arin Sarkissian <arin@skook.local>
2009-06-07 12:10:50 -0700

    free.php

---
+++ www/free.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>Helping You Search Craigslist - Craig's Little Buddy</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="A better way to find stuff on craigslist: Search multiple craigslist cities at the same time, sort and browser results." />
+        <meta name="keywords" content="craigslist, search, craigslist search, for sale, WTB" />
+        <meta name="verify-v1" content="BfbaMc3dkDvTDVaqM6p36ELw1XManUY4RwidzYJWvjM=" />
+                
+        <link type="text/css" rel="stylesheet" href="/_/css/style.css" />
+        <link type="text/css" rel="stylesheet" href="/_/css/home.css" />
+                        
+                    </head>
+    
+    <body>
+        <div id="content">
+            <div id="main">
+                <div class="homeBg">
+    <div class="frm">
+        <form action="#" method="GET" onsubmit="return false">
+            <input class="txt" value="i've been banned by craigslist" disabled="true" style="color: grey" maxlength="255" name="q" type="text" value="" />
+            <button class="homeSearch"></button>
+        </form>    
+    </div>
+</div>
+
+<div>
+    <h2>Bye, Bye Folks!!!</h2>
+    
+    <div style="font-size: 13px; padding: 0 4px 0 4px">
+        <p>
+            Alright. As a bunch of you know the site's been cut off access to craigslist's search results a few times already.
+            It sucks, but, eh, such is life. In the past I've done a few things to try and play nicer with craigslist so that
+            subsequent bannings don't happen & that's kinda sorta worked but eventually the site's been banned again.
+        </p>
+        
+        <p>
+            I really can't "make" craigslist be nice & allow the site access to their feeds. hell, it's their data and they can enforce their TOS any way they want. I could
+            do some shady-ish things but that's a losing battle & something I don't wanna get into.
+        </p>
+        
+        <p>
+            So, it's time to day good-bye... but, I'm not gonna leave you hanging. As a parting gift I'm leaving you with all the code
+            for this site. It's hacky & crappy but it works. So, go ahead and run the site at home, on your personal machine, for your own personal use.
+        </p>
+        
+        <p align="center">
+            <a href="http://github.com/phatduckk/craigslittlebuddy.com/tree/master"><img src="http://github.com/images/modules/download/zip.png" /></a>
+            <br />
+            <a href="http://github.com/phatduckk/craigslittlebuddy.com/tree/master">Download all the source for this site at GitHub</a>
+        </p>
+        
+        <p>
+            So, thanks to all the folks who emailed me telling me how much they dug the site. I'm glad I was able to help =)
+        </p>
+        
+        <p>
+            Woot!<br />
+            - Arin
+        </p>
+    </div>
+</div>
+
+</div>
+            
+            <div id="footer">
+    <a href="/home/about">About</a> 
+    <a href="/home/contact">Contact</a>
+    <a href="/home/faq">FAQ</a>
+    <a href="/home/sitemap">Sitemap</a>
+    <a href="/home/random">Say Something Random</a>
+    <a href="http://www.phatduckk.com/blog/craigs-little-buddy">Blog</a>
+    &copy; 2009 Arin Sarkissian
+</div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3038848-2");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>        </div>
+        
+    </body>
+</html>
